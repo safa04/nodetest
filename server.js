2850084777c36588ba672ef9cb4b3cc1b4e6a4f3
Salvador Flores <salvadorflores@Salvadors-MacBook-Pro.local>
2017-09-14 17:26:48 -0500

    as

--- server.js
+++ server.js
@@ -12,13 +12,11 @@ app.use(express.static(__dirname + '/client'));
 
 /*Allow CORS*/
 app.use(function(req, res, next) {
-    
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization'); 
    res.setHeader('Access-Control-Allow-Methods', '*');
    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
    res.setHeader('Access-Control-Max-Age', '1000');
-     
    next();
 });
 
