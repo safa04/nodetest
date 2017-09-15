d731bbfd448007b6d2de6d98ecdcee08a72bce30
Salvador Flores <salvadorflores@Salvadors-MacBook-Pro.local>
2017-09-15 15:16:37 -0500

    s

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.all('*', function (req, res, next) {
             res.send(500, { error: 'There is no Target-Endpoint header in the request' });
             return;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('X-Authorization'), 'Content-Type' : 'application/json'} },
             function (error, response, body) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
